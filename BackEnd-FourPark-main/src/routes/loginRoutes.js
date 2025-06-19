9734dadfa2c50194011508ea72ed56131510add4
lopezander <72524904+lopezander@users.noreply.github.com>
2025-06-19 18:12:55 -0500

    Delete BackEnd-FourPark-main directory

--- BackEnd-FourPark-main/src/routes/loginRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const loginController = require("../controllers/loginController");
-
-router.post("/loginCliente", loginController.loginCliente);
-//router.get('/loginGerente', loginController.loginGerente);
-
-module.exports = router;
